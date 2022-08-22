f570c4f2e60a5a9a80c8f650a431c8dd1c3544b3
Dwi <1472909+luneareth@users.noreply.github.com>
2022-08-22 12:56:20 +0700

    Update nhc.sh

--- nhc/nhc.sh
+++ nhc/nhc.sh
@@ -2,10 +2,11 @@
 
 
 CONFYAML="roomit-aptos-checker"
+DNSNAME=""
 
 
 mkdir -p conf
-docker run -v $(pwd)/conf:/nhc -t aptoslabs/node-checker:nightly /usr/local/bin/aptos-node-checker configuration create --configuration-name ${CONFYAML} --configuration-name-pretty "RoomIT Aptos Nodes" --url http://cikuray.roomit.xyz/ --evaluators consensus_proposals consensus_round consensus_timeouts api_latency network_peers_within_tolerance | tee conf/${CONFYAML}.yaml
+docker run -v $(pwd)/conf:/nhc -t aptoslabs/node-checker:nightly /usr/local/bin/aptos-node-checker configuration create --configuration-name ${CONFYAML} --configuration-name-pretty "RoomIT Aptos Nodes" --url http://${DNSNAME} --evaluators consensus_proposals consensus_round consensus_timeouts api_latency network_peers_within_tolerance | tee conf/${CONFYAML}.yaml
 docker stop ${CONFYAML}
 docker rm ${CONFYAML}
 docker run -d --name=${CONFYAML} -p 20121:20121 -v $(pwd)/conf:/nhc -t aptoslabs/node-checker:nightly /usr/local/bin/aptos-node-checker server run --baseline-node-config-paths /nhc/${CONFYAML}.yaml
