785960ab389616f40ac652b2bae19fc6f21bc5a5
Dwi <1472909+luneareth@users.noreply.github.com>
2022-08-22 20:44:09 +0700

    Update nhc.sh

--- nhc/nhc.sh
+++ nhc/nhc.sh
@@ -12,7 +12,7 @@ OPTIONS=$1
 function help(){
 echo -e "
 1. Running NHC
-nhc.sh [start
+nhc.sh start
 2. Generate Config NHC
 nhc.sh generate
 3. Start Container
@@ -23,7 +23,7 @@ function nhc:config(){
 mkdir -p conf
 if [ ${KINDNODE} == "validator" ]
 then
-docker run -v $(pwd)/conf:/nhc -t aptoslabs/node-checker:nightly /usr/local/bin/aptos-node-checker configuration create --configuration-name ${CONFYAML} --configuration-name-pretty "${CONFYAMLPRETTY}" --url ${URL} --chain-id ${CHAINID} --role-type validator --evaluators  consensus_proposals api_latency consensus_round api_transaction_availability
+docker run -v $(pwd)/conf:/nhc -t aptoslabs/node-checker:nightly /usr/local/bin/aptos-node-checker configuration create --configuration-name ${CONFYAML} --configuration-name-pretty "${CONFYAMLPRETTY}" --url ${URL} --chain-id ${CHAINID} --role-type validator --evaluators  consensus_proposals api_latency consensus_round api_transaction_availability | tee conf/${CONFYAML}.yaml
 else 
 docker run -v $(pwd)/conf:/nhc -t aptoslabs/node-checker:nightly /usr/local/bin/aptos-node-checker configuration create --configuration-name ${CONFYAML} --configuration-name-pretty "RoomIT Aptos Nodes" --url http://cikuray.roomit.xyz/ --evaluators consensus_proposals consensus_round consensus_timeouts api_latency network_peers_within_tolerance | tee conf/${CONFYAML}.yaml
 fi
